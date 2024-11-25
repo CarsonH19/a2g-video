bc109f91774b7ce4517663fe85cb4824f71cba91
CarsonH19 <carsonharp.fullstack@gmail.com>
2024-11-25 16:23:57 -0500

    fix: logo

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
-// import { Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-// import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
-// const font = Poppins({
-//   subsets: ["latin"],
-//   weight: ["200", "300", "400", "500", "600", "700", "800"],
-// });
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1">
-          <Image src={"a2g.svg"} alt="A2G Logo" height={52} width={52} />
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+          <Image src={"a2g.svg"} alt="A2G Logo" height={62} width={62} />
+        </div>
+        <div className={cn("hidden lg:block", font.className)}>
+          <p className="text-lg font-semibold">A2G International</p>
+          <p className="text-xs text-muted-foreground">Vision</p>
         </div>
       </div>
     </Link>
