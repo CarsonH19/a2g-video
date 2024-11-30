dfe9b8af81bb498f9aa7ca3f0014d57407c3fd1c
CarsonH19 <carsonharp.fullstack@gmail.com>
2024-11-30 18:31:35 -0500

    feat: sidebar

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import { Wrapper } from "./wrapper";
+import { Toggle } from "./toggle";
+
+export const Sidebar = () => {
+  // fetch users
+
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
